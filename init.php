67c7aebb6b5daa6d80fd55693cefccee66dfadd9
Alejandro Campos <alejandrocampossampedro@gmail.com>
2025-12-11 11:33:05 +0100

    "Copia de seguridad subida con éxito"

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+
+//timezone
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// conexão com o banco de dados
+
+define('BD_SERVIDOR','sql100.infinityfree.com');
+define('BD_USUARIO','if0_40649021');
+define('BD_SENHA','contraseña');
+define('BD_BANCO','if0_40649021_examen2025');
+
+
---
+++ script.sql
@@ -0,0 +1,8 @@
+CREATE TABLE livros (
+    nome          VARCHAR(255) NOT NULL,
+    autor         VARCHAR(255),
+    quantidade    INT NOT NULL,
+    preco         VARCHAR(255) NOT NULL,
+    flag          TINYINT(1) NULL DEFAULT 0,
+    data          DATE NOT NULL,
+PRIMARY KEY (nome))
